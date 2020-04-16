7d25608442367cd86c464b622c0e573442c3cce6
yycho0108 <jchocholate@gmail.com>
2020-04-16 13:53:00 -0400

    bump minor version

--- cho_util/core/include/cho_util/core/version.hpp
+++ cho_util/core/include/cho_util/core/version.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define CU_VERSION_MAJOR 0
-#define CU_VERSION_MINOR 1
+#define CU_VERSION_MINOR 2
 #define CU_VERSION_PATCH 0
 #define CU_VERSION \
   CU_VERSION_MAJOR##"."##CU_VERSION_MINOR##"."##CU_VERSION_PATCH

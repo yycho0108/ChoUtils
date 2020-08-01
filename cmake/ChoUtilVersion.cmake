e451648bdfb446e2bbcff97d62017f4833f7923b
yycho0108 <jchocholate@gmail.com>
2020-08-01 17:00:11 -0400

    fixed cmake

--- cmake/ChoUtilVersion.cmake
+++ cmake/ChoUtilVersion.cmake
@@ -1,6 +1,12 @@
-SET(CU_VERSION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/cho_util/core/include/cho_util/core/version.hpp")
-file(STRINGS "${CU_VERSION_FILE}" CU_VERSION_PARTS REGEX "#define CU_VERSION_[A-Z]+[ ]+" )
+set(CHO_VERSION_FILE
+    "${CMAKE_CURRENT_SOURCE_DIR}/cho_util/core/include/cho_util/core/version.hpp"
+)
+file(STRINGS "${CHO_VERSION_FILE}" CHO_VERSION_PARTS
+     REGEX "#define CHO_VERSION_[A-Z]+[ ]+")
 
-string(REGEX REPLACE ".+CU_VERSION_MAJOR[ ]+([0-9]+).*" "\\1" CU_VERSION_MAJOR "${CU_VERSION_PARTS}")
-string(REGEX REPLACE ".+CU_VERSION_MINOR[ ]+([0-9]+).*" "\\1" CU_VERSION_MINOR "${CU_VERSION_PARTS}")
-string(REGEX REPLACE ".+CU_VERSION_PATCH[ ]+([0-9]+).*" "\\1" CU_VERSION_PATCH "${CU_VERSION_PARTS}")
+string(REGEX REPLACE ".+CHO_VERSION_MAJOR[ ]+([0-9]+).*" "\\1"
+                     CHO_VERSION_MAJOR "${CHO_VERSION_PARTS}")
+string(REGEX REPLACE ".+CHO_VERSION_MINOR[ ]+([0-9]+).*" "\\1"
+                     CHO_VERSION_MINOR "${CHO_VERSION_PARTS}")
+string(REGEX REPLACE ".+CHO_VERSION_PATCH[ ]+([0-9]+).*" "\\1"
+                     CHO_VERSION_PATCH "${CHO_VERSION_PARTS}")

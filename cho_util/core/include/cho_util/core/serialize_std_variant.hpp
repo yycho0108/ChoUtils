fd7146be224ac0ce0cc06618bbd6bff6ecb20acd
yycho0108 <jchocholate@gmail.com>
2020-08-02 14:36:06 -0400

    cleanup expression

--- cho_util/core/include/cho_util/core/serialize_std_variant.hpp
+++ cho_util/core/include/cho_util/core/serialize_std_variant.hpp
@@ -8,6 +8,9 @@ namespace serialization {
 template <class Archive, typename... Ts>
 void save(Archive& ar, std::variant<Ts...> const& v, unsigned int /*version*/
 ) {
+  // NOTE(yycho0108): It is important to explicitly set the datatype of `which`
+  // here. Carelessly assigning it to something like an `int` will result in
+  // incorrect serialization due to inconsistency between load/save.
   const std::size_t which = v.index();
   ar << which;
   std::visit([&ar](const auto& v) { ar << v; }, v);
@@ -15,14 +18,13 @@ void save(Archive& ar, std::variant<Ts...> const& v, unsigned int /*version*/
 
 namespace detail {
 template <class Archive, typename Variant, std::size_t... Is>
-void load_variant_at_index(Archive& ar, Variant* const v,
-                           const std::size_t which,
-                           std::index_sequence<Is...>) {
+void load_variant_impl(Archive& ar, Variant* const v, const std::size_t which,
+                       std::index_sequence<Is...>) {
   // Default construct the variant and deserialize into concrete type.
   std::initializer_list<int>(
-      {(which == Is ? (*v = std::variant_alternative_t<Is, Variant>(),
-                       (ar >> std::get<Is>(*v))),
-        0 : 0)...});
+      {(which == Is ? ((void)(*v = std::variant_alternative_t<Is, Variant>()),
+                       (void)(ar >> std::get<Is>(*v)), 0)
+                    : 0)...});
 }
 }  // namespace detail
 
@@ -30,7 +32,7 @@ template <class Archive, typename... Ts>
 void load(Archive& ar, std::variant<Ts...>& v, const unsigned int /*version*/) {
   std::size_t which;
   ar >> which;
-  return detail::load_variant_at_index<Archive, std::variant<Ts...>>(
+  return detail::load_variant_impl<Archive, std::variant<Ts...>>(
       ar, &v, which, std::make_index_sequence<sizeof...(Ts)>{});
 }
 
